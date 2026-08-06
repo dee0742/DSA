e9b835e858efbadcdb8587a6a583599f6c1f379e
Dino Kawade <dinokawade9@gmail.com>
2026-08-06 13:21:56 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (33.20%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2 , nums1);
+        }
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int left = 0;
+        int right = n;
+
+        while(left <= right){
+            int partitionX = (left + right) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == n) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == m) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if((m + n) % 2 == 0){
+                    return (Math.max(maxLeftX , maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                }else {
+                    return Math.max(maxLeftX , maxLeftY);
+                }
+                }else if (maxLeftX > minRightY){
+                    right = partitionX - 1;
+                }else{
+                    left = partitionX + 1;
+                }
+            }
+            throw new IllegalArgumentException("Input arrays are not sorted");
+        }
+    } 
+
