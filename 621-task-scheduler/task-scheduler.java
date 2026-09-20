5489cbe59998f380e3d914b3a4ecaea81cc6bda4
Dino Kawade <dinokawade9@gmail.com>
2026-09-20 15:18:15 +0530

    Time: 29 ms (32.71%) | Memory: 48 MB (77.58%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26]; 
+
+        for(char task : tasks){
+            freq[task - 'A']++;
+        }
+      
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int f : freq){
+            if(f > 0){
+                pq.offer(f);
+            }
+        }
+        int time = 0;
+
+        while(!pq.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            int cycle = n + 1;
+
+            while(cycle > 0 && !pq.isEmpty()){
+                int current = pq.poll();
+                current--;
+                if(current > 0){
+                    temp.add(current);
+                }
+                time++;
+                cycle--;
+            }
+            for(int remaining : temp){
+                pq.offer(remaining);
+            }
+            if(!pq.isEmpty()){
+                time += cycle;
+            }
+        }
+        return time;
+    }
+}
